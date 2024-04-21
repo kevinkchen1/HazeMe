ec9b588d93a6ee7397a5072928b5a23349319474
ashwinchembu <ashchembu@gmail.com>
2024-04-21 03:23:51 -0700

    uoh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-blue-700 h-screen flex flex-col items-center justify-between p-4">
-      
+    <div className="bg-blue-500 flex flex-col items-center justify-between text-white hover:bg-blue-500">  
       <img src="wouldja.png" alt="Centered Image" className="self-center"/>
-      <img src="dead.png" alt="Centered Image" className="self-center"/>
+      <img src="mobile.png" alt="Centered Image" className="self-center"/>
       <button 
-        className="bg-white text-3xl text-blue-700 font-extrabold py-20 px-40 rounded-3xl mb-4"
+        className="bg-white text-xl text-blue-500 font-bold py-2 px-8 rounded-xl mb-4"
+        style={{}}
         onClick={() => navigate('/start')}
       >
         LETS GO
